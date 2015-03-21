2f2f15b04c503934db308efd8c3c07c0da20cadb
Pedro <pbarreto@bacardi.com>
2015-03-21 20:33:43 +0000

    added morgan for logs

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var morgan = require('morgan');
 
 var app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', function(req, res){
   res.send('Hello from inside a container!');
